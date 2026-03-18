277918007e20c93c5784922b92c174d9f4c5b6ee
Alina Gaiduk <gaidukalina18@yandex.ru>
2026-03-18 13:04:31 +0300

    [feat]]: add note port

---
+++ internal/application/ports/note.go
@@ -0,0 +1,19 @@
+package ports
+
+import (
+	"context"
+	"time"
+)
+
+type InternalNoteRepository interface {
+	Create(ctx context.Context, n InternalNoteRecord) error
+	ListByReport(ctx context.Context, reportID string, limit int, offset int) ([]InternalNoteRecord, int, error)
+}
+
+type InternalNoteRecord struct {
+	ID                string
+	ReportID          string
+	AuthorModeratorID string
+	Text              string
+	CreatedAt         time.Time
+}
